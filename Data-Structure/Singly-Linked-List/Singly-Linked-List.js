da917c9187c6874ea36c56b572e293e7f2e80e50
Suplob Roy <parbaroy30@gmail.com>
2022-01-22 21:12:11 +0600

    insert-method-added

--- Data-Structure/Singly-Linked-List/Singly-Linked-List.js
+++ Data-Structure/Singly-Linked-List/Singly-Linked-List.js
@@ -77,4 +77,19 @@ class SinglyLinkedList {
         }
         return false
     }
+    insert(val, index) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val);
+        if (index === 0) return !!this.unshift(val)
+        let newNode = new Node(val);
+        let prev = this.get(index - 1)
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
 }
+
+const list = new SinglyLinkedList()
+
