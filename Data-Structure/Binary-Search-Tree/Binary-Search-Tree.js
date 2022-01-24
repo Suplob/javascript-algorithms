97799975601a07587cc5e39240b5398c89b7aee5
Suplob Roy <parbaroy30@gmail.com>
2022-01-24 17:19:36 +0600

    BST BFS

--- Data-Structure/Binary-Search-Tree/Binary-Search-Tree.js
+++ Data-Structure/Binary-Search-Tree/Binary-Search-Tree.js
@@ -50,4 +50,30 @@ class BinarySearchTree {
         }
         return !found ? undefined : current;
     }
+    BreadthFirstSearch() {
+        let node = this.root,
+            queue = [],
+            data = [];
+
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
+
+
+const tree = new BinarySearchTree();
+
+tree.insert(10)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+
+console.log(tree.BreadthFirstSearch());
