4b5cbf423b82ca29713194aef41f22c86772fcc9
Suplob Roy <parbaroy30@gmail.com>
2022-01-24 11:59:33 +0600

    stack-with-linked-list-added

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,5 +1,4 @@
-// Last in first out (LIFO)
-
+// Stack with array
 class Stack {
   constructor() {
     this.stack = [];
@@ -9,5 +8,44 @@ class Stack {
   }
   remove() {
     return this.stack.pop();
+  }
+}
+
+// Stack with linked list
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class StackWithLinkedList {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    const nowNode = new Node(val);
+    if (!this.head) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    const removedNode = this.head;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+
+    this.size--;
+    return removedNode.val;
   }
 }
