231401e0bd2f818d5e3d51a64174eebda7182fcf
Suplob Roy <parbaroy30@gmail.com>
2022-01-24 16:05:10 +0600

    BST insert

---
+++ Data-Structure/Binary-Search-Tree/Binary-Search-Tree.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(val) {
+        const newNode = new Node(val);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (val === current.val) return undefined;
+            if (val < current.val) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+console.log(tree);
